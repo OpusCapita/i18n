4c2e6796472f9867e8bf03770e6cc760f0e327ee
Michael Pritchin <michael.pritchin@jcatalog.com>
2016-04-27 15:30:49 +0300

    (PIM-10996) Added name field for ParseError

--- src/converters/ParseError.js
+++ src/converters/ParseError.js
@@ -1,5 +1,6 @@
 function ParseError(errorCode, errorArgs) {
   Error.call(this);
+  this.name = 'ParseError';
   this.errorCode = errorCode;
   this.errorArgs = errorArgs;
   this.message = `invalid parsed value [${errorArgs.value}]`;
