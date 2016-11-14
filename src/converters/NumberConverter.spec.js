3a8972a94121ea5b54a1518517a3c9487f398e01
volkovich <Kirill.Volkovich@jCatalog.com>
2016-11-14 15:31:50 +0300

    Improved tests of NumberConverter.

--- src/converters/NumberConverter.spec.js
+++ src/converters/NumberConverter.spec.js
@@ -16,20 +16,20 @@ describe('NumberConverter', () => {
     assert.throws(() => {return dc.valueToString('werwe')}, TypeError, `'werwe' is not a Number!`);
 
     assert.strictEqual(dc.stringToValue(null), null);
-    assert.equal(dc.stringToValue('10,000.00'), 10000);
-    assert.equal(dc.stringToValue('-10,000.00'), -10000);
-    assert.equal(dc.stringToValue('1,100.99'), 1100.99);
+    assert.strictEqual(dc.stringToValue('10,000.00'), 10000);
+    assert.strictEqual(dc.stringToValue('-10,000.00'), -10000);
+    assert.strictEqual(dc.stringToValue('1,100.99'), 1100.99);
     assert.throws(() => {return dc.stringToValue(12345)}, TypeError, `'12345' is not a String!`);
 
     dc = new NumberConverter('#,##0.00', null, '.');
 
-    assert.equal(dc.valueToString(10000), '10000.00');
-    assert.equal(dc.valueToString(-10000), '-10000.00');
-    assert.equal(dc.valueToString(1100.99), '1100.99');
+    assert.strictEqual(dc.valueToString(10000), '10000.00');
+    assert.strictEqual(dc.valueToString(-10000), '-10000.00');
+    assert.strictEqual(dc.valueToString(1100.99), '1100.99');
 
-    assert.equal(dc.stringToValue('10000.00'), 10000);
-    assert.equal(dc.stringToValue('-10000.00'), -10000);
-    assert.equal(dc.stringToValue('1100.99'), 1100.99);
+    assert.strictEqual(dc.stringToValue('10000.00'), 10000);
+    assert.strictEqual(dc.stringToValue('-10000.00'), -10000);
+    assert.strictEqual(dc.stringToValue('1100.99'), 1100.99);
 
     const badValue = '10,000.00';
     assert.throws(() => {
@@ -38,15 +38,15 @@ describe('NumberConverter', () => {
 
     dc = new NumberConverter('#,##0.00', ' ', ',');
 
-    assert.equal(dc.valueToString(10000), '10 000,00');
-    assert.equal(dc.valueToString(-10000), '-10 000,00');
-    assert.equal(dc.valueToString(1100.55), '1 100,55');
-    assert.equal(dc.valueToString(-1.1), '-1,10');
-    assert.equal(dc.valueToString(-1.9), '-1,90');
+    assert.strictEqual(dc.valueToString(10000), '10 000,00');
+    assert.strictEqual(dc.valueToString(-10000), '-10 000,00');
+    assert.strictEqual(dc.valueToString(1100.55), '1 100,55');
+    assert.strictEqual(dc.valueToString(-1.1), '-1,10');
+    assert.strictEqual(dc.valueToString(-1.9), '-1,90');
 
-    assert.equal(dc.stringToValue('10 000,00'), 10000);
-    assert.equal(dc.stringToValue('-10 000,00'), -10000);
-    assert.equal(dc.stringToValue('1 100,99'), 1100.99);
+    assert.strictEqual(dc.stringToValue('10 000,00'), 10000);
+    assert.strictEqual(dc.stringToValue('-10 000,00'), -10000);
+    assert.strictEqual(dc.stringToValue('1 100,99'), 1100.99);
 
     const invalidValues = ['test', 'test1321321', '10,000.00', '5435432test'];
 
@@ -62,10 +62,10 @@ describe('NumberConverter', () => {
     // formating integer values
     dc = new NumberConverter('#,##0', '`');
 
-    assert.equal(dc.valueToString(10000), '10`000');
-    assert.equal(dc.stringToValue('10`000'), 10000);
+    assert.strictEqual(dc.valueToString(10000), '10`000');
+    assert.strictEqual(dc.stringToValue('10`000'), 10000);
 
-    assert.equal(dc.valueToString(10000.99), '10`001');
+    assert.strictEqual(dc.valueToString(10000.99), '10`001');
 
     assert.throws(() => {
       dc.stringToValue('10`000.99');
@@ -73,38 +73,38 @@ describe('NumberConverter', () => {
 
     dc = new NumberConverter('#.##', null, ',');
 
-    assert.equal(dc.valueToString(100), '100');
+    assert.strictEqual(dc.valueToString(100), '100');
 
-    assert.equal(dc.stringToValue('1000'), 1000);
-    assert.equal(dc.stringToValue('100000,1'), 100000.1);
+    assert.strictEqual(dc.stringToValue('1000'), 1000);
+    assert.strictEqual(dc.stringToValue('100000,1'), 100000.1);
 
-    assert.equal(dc.valueToString(100.01), '100,01');
+    assert.strictEqual(dc.valueToString(100.01), '100,01');
 
-    assert.equal(dc.valueToString(0.0), '0');
-    assert.equal(dc.valueToString(0), '0');
+    assert.strictEqual(dc.valueToString(0.0), '0');
+    assert.strictEqual(dc.valueToString(0), '0');
 
     dc = new NumberConverter('#.##', null, ',', true);
 
-    assert.equal(dc.valueToString(100), '100,');
+    assert.strictEqual(dc.valueToString(100), '100,');
 
-    assert.equal(dc.stringToValue('1000'), 1000);
-    assert.equal(dc.stringToValue('100000,1'), 100000.1);
+    assert.strictEqual(dc.stringToValue('1000'), 1000);
+    assert.strictEqual(dc.stringToValue('100000,1'), 100000.1);
 
-    assert.equal(dc.valueToString(100.01), '100,01');
+    assert.strictEqual(dc.valueToString(100.01), '100,01');
 
-    assert.equal(dc.valueToString(0.0), '0,');
-    assert.equal(dc.valueToString(0.1), '0,1');
+    assert.strictEqual(dc.valueToString(0.0), '0,');
+    assert.strictEqual(dc.valueToString(0.1), '0,1');
 
     dc = new NumberConverter('#.##########', null, ',', false);
-    assert.equal(dc.valueToString(1000000), '1000000');
+    assert.strictEqual(dc.valueToString(1000000), '1000000');
 
     dc = new NumberConverter('#,##0.0#########', ',', '.');
-    assert.equal(dc.valueToString(123456789.12), '123,456,789.12');
+    assert.strictEqual(dc.valueToString(123456789.12), '123,456,789.12');
 
     // throw exceptions on large integers
     assert.throws(() => dc.valueToString(12345678912345678910), AccuracyError);
 
     dc = new NumberConverter('00', null, '.');
-    assert.equal(dc.valueToString(9), '09');
+    assert.strictEqual(dc.valueToString(9), '09');
   });
 });
